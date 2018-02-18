f24dcab483dff93d2c972229915cfe475846e1af
antonio <antoniousjacoub@gmail.com>
2018-02-18 20:38:35 +0200

    ulits

--- app/src/main/java/com/example/antonio/multipleitemslist2/Ulits.java
+++ app/src/main/java/com/example/antonio/multipleitemslist2/Ulits.java
@@ -17,4 +17,35 @@ public class Ulits {
     //mDrawerToggle.syncState();
     //for videoplayer
     //https://github.com/afollestad/easy-video-player
+
+//    private void implementScrollListener() {
+//        listView.setOnScrollListener(new OnScrollListener() {
+//
+//            @Override
+//            public void onScrollStateChanged(AbsListView arg0, int scrollState) {
+//                // If scroll state is touch scroll then set userScrolled
+//                // true
+//                if (scrollState == OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+//                    userScrolled = true;
+//
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onScroll(AbsListView view, int firstVisibleItem,
+//                                 int visibleItemCount, int totalItemCount) {
+//                // Now check if userScrolled is true and also check if
+//                // the item is end then update list view and set
+//                // userScrolled to false
+//                if (userScrolled
+//                        && firstVisibleItem + visibleItemCount == totalItemCount) {
+//
+//                    userScrolled = false;
+//                    updateListView();
+//                }
+//            }
+//        });
+//    }
+//
 }
